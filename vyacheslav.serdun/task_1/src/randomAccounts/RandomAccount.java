f9e4ed741322e32d51236468ed3f2d3c91f84624
Vadim Martsun <vadimmartsun@gmail.com>
2019-10-16 19:16:11 +0300

    Add a class for generating collections with random accounts

---
+++ vyacheslav.serdun/task_1/src/randomAccounts/RandomAccount.java
@@ -0,0 +1,63 @@
+package randomAccounts;
+
+import pojos.Accounts;
+import pojos.BankAccount;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+
+public class RandomAccount {
+    private static final String ALPHABET = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz";
+
+    private Random random;
+
+    public RandomAccount() {
+        random = new Random();
+    }
+
+    /**
+     * Generates a String with random characters in it.
+     *
+     * @return a line with a random length but no more than 52 and with random
+     * characters.
+     */
+    private String generateRandomLoginName() {
+        StringBuilder result = new StringBuilder();
+        for (int i = 0; i < random.nextInt(ALPHABET.length()); i++) {
+            result.append(ALPHABET.charAt(random.nextInt(ALPHABET.length())));
+        }
+        return result.toString();
+    }
+
+    public Accounts generateAccounts(int count) {
+        if (count <= 1)
+            throw new IllegalArgumentException("Count should not be less or equal to 1");
+
+        Accounts accounts = new Accounts();
+        List<BankAccount> collA = new ArrayList<>();
+        List<BankAccount> collB = new ArrayList<>();
+
+        BankAccount account;
+        boolean duplicated;
+
+        while (count > 0) {
+            duplicated = random.nextBoolean();
+            if (duplicated) {
+                account = new BankAccount(Math.abs(random.nextInt()), generateRandomLoginName(), random.nextInt());
+                collA.add(account);
+                collB.add(account);
+            } else {
+                collA.add(new BankAccount(Math.abs(random.nextInt()), generateRandomLoginName(), random.nextInt()));
+                collB.add(new BankAccount(Math.abs(random.nextInt()), generateRandomLoginName(), random.nextInt()));
+            }
+            count--;
+        }
+
+        Collections.shuffle(collA);
+        accounts.setAccountsA(collA);
+        accounts.setAccountsB(collB);
+        return accounts;
+    }
+}
