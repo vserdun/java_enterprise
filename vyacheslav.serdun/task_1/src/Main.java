0f7675a80fa30b1749733a376e654cf128712c2f
Vadim Martsun <vadimmartsun@gmail.com>
2019-10-16 19:21:43 +0300

    Add some code for testing

--- vyacheslav.serdun/task_1/src/Main.java
+++ vyacheslav.serdun/task_1/src/Main.java
@@ -1,6 +1,37 @@
+import accountManager.AccountManager;
+import pojos.Accounts;
+import pojos.BankAccount;
+import randomAccounts.RandomAccount;
+
+import java.util.List;
+import java.util.logging.Logger;
+
 public class Main {
 
+    private static Logger logger = Logger.getLogger(Main.class.getSimpleName());
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        RandomAccount randomAccount = new RandomAccount();
+
+        Accounts accounts = randomAccount.generateAccounts(100000);
+
+        logger.info("Generated collection A: ");
+        accounts.getAccountsA().forEach(System.out::println);
+
+        logger.info("Generated collection B: ");
+        accounts.getAccountsB().forEach(System.out::println);
+
+        long start = System.currentTimeMillis();
+        List<BankAccount> result = AccountManager.findDuplicates(accounts.getAccountsA(), accounts.getAccountsB());
+        long finish = System.currentTimeMillis();
+
+        long time = (finish - start);
+
+        logger.info("Duplicates: ");
+        result.forEach(System.out::println);
+
+        logger.info("Found " + result.size() + " duplicate elements.");
+
+        logger.info("\nTime spent: " + time + " milliseconds.");
     }
 }
