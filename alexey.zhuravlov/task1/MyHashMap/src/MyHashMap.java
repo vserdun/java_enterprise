97db857d5025811aab9a56e817c6ddfd8ce70f85
alexzhravlov <alexzhuravlov13@gmail.com>
2019-10-17 21:24:17 +0300

    3 types of findduplicates + fix code style

--- alexey.zhuravlov/task1/MyHashMap/src/MyHashMap.java
+++ alexey.zhuravlov/task1/MyHashMap/src/MyHashMap.java
@@ -1,53 +1,46 @@
 import java.util.Objects;
 
-public class MyHashMap
-{
-  private MyHashNode[] table;
-  int size;
+public class MyHashMap {
+    private MyHashNode[] table;
+    int size;
 
-  public void put(String key, String value)
-  {
-    int hash = Objects.hash(key);
-    int index = hash % (size - 1);
+    public void put(String key, String value) {
+        int hash = Objects.hash(key);
+        int index = hash % (size - 1);
 
-    MyHashNode currentEmptyNode = table[index];
+        MyHashNode currentEmptyNode = table[index];
 
-    while (currentEmptyNode != null)
-    {
-      currentEmptyNode = currentEmptyNode.getNext();
-    }
-    currentEmptyNode.setHash(hash);
-    currentEmptyNode.setKey(key);
-    currentEmptyNode.setValue(value);
-    currentEmptyNode.setNext(null);
+        while (currentEmptyNode != null) {
+            currentEmptyNode = currentEmptyNode.getNext();
+        }
+        currentEmptyNode.setHash(hash);
+        currentEmptyNode.setKey(key);
+        currentEmptyNode.setValue(value);
+        currentEmptyNode.setNext(null);
 
-  }
-
-  public String get(String key)
-  {
-    int hash = Objects.hash(key);
-    int index = hash % (size - 1);
-
-    MyHashNode currentNode = table[index];
-
-    while (currentNode.getHash() != hash)
-    {
-      currentNode = currentNode.getNext();
     }
 
-    while (!currentNode.getKey().equals(key))
-    {
-      currentNode = currentNode.getNext();
+    public String get(String key) {
+        int hash = Objects.hash(key);
+        int index = hash % (size - 1);
+
+        MyHashNode currentNode = table[index];
+
+        while (currentNode.getHash() != hash) {
+            currentNode = currentNode.getNext();
+        }
+
+        while (!currentNode.getKey().equals(key)) {
+            currentNode = currentNode.getNext();
+        }
+        return currentNode.getValue();
+
     }
-    return currentNode.getValue();
 
-  }
-
-  public void hash(MyHashNode myHashNode)
-  {
-    int hash = Objects.hash(myHashNode.getKey());
-    myHashNode.setHash(hash);
-  }
+    public void hash(MyHashNode myHashNode) {
+        int hash = Objects.hash(myHashNode.getKey());
+        myHashNode.setHash(hash);
+    }
 
 }
 
--- alexey.zhuravlov/task1/MyHashMap/src/MyHashNode.java
+++ alexey.zhuravlov/task1/MyHashMap/src/MyHashNode.java
@@ -1,71 +1,58 @@
 import java.util.Objects;
 
-public class MyHashNode
-{
-  private int hash;
-  private String key;
-  private String value;
-  private MyHashNode next;
+public class MyHashNode {
+    private int hash;
+    private String key;
+    private String value;
+    private MyHashNode next;
 
-  public int getHash()
-  {
-    return hash;
-  }
-
-  public void setHash(int hash)
-  {
-    this.hash = hash;
-  }
-
-  public String getKey()
-  {
-    return key;
-  }
-
-  public void setKey(String key)
-  {
-    this.key = key;
-  }
-
-  public String getValue()
-  {
-    return value;
-  }
-
-  public void setValue(String value)
-  {
-    this.value = value;
-  }
-
-  public MyHashNode getNext()
-  {
-    return next;
-  }
-
-  public void setNext(MyHashNode next)
-  {
-    this.next = next;
-  }
-
-  @Override
-  public boolean equals(Object o)
-  {
-    if (this == o)
-    {
-      return true;
+    public int getHash() {
+        return hash;
     }
-    if (o == null || getClass() != o.getClass())
-    {
-      return false;
-    }
-    MyHashNode that = (MyHashNode) o;
-    return Objects.equals(key, that.key) &&
-        Objects.equals(value, that.value);
-  }
 
-  @Override
-  public int hashCode()
-  {
-    return Objects.hash(key, value);
-  }
+    public void setHash(int hash) {
+        this.hash = hash;
+    }
+
+    public String getKey() {
+        return key;
+    }
+
+    public void setKey(String key) {
+        this.key = key;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    public MyHashNode getNext() {
+        return next;
+    }
+
+    public void setNext(MyHashNode next) {
+        this.next = next;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) {
+            return true;
+        }
+        if (o == null || getClass() != o.getClass()) {
+            return false;
+        }
+        MyHashNode that = (MyHashNode) o;
+        return Objects.equals(key, that.key) &&
+                Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(key, value);
+    }
 }
