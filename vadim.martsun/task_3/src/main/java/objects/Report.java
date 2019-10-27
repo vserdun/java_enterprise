ed725995c5fea89b5f18f0925d497bf863b95e39
Vadim Martsun <vadimmartsun@gmail.com>
2019-10-27 15:52:16 +0300

    Temporary remove Lombok

--- vadim.martsun/task_3/src/main/java/objects/Report.java
+++ vadim.martsun/task_3/src/main/java/objects/Report.java
@@ -1,20 +1,37 @@
 package objects;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
-
 import java.time.LocalDate;
+import java.util.Objects;
 
-@ToString
-@AllArgsConstructor
-@Data
-@EqualsAndHashCode
 public class Report {
     private int id;
     private LocalDate date;
     private String name;
     private String description;
     private Status status;
+
+    public Report(int id, LocalDate date, String name, String description, Status status) {
+        this.id = id;
+        this.date = date;
+        this.name = name;
+        this.description = description;
+        this.status = status;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Report report = (Report) o;
+        return id == report.id &&
+                date.equals(report.date) &&
+                name.equals(report.name) &&
+                description.equals(report.description) &&
+                status == report.status;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, date, name, description, status);
+    }
 }
