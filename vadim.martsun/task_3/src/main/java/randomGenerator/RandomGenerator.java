b0e4f9ff212e894894b1af7245d543f14bcd04fc
Vadim Martsun <vadimmartsun@gmail.com>
2019-10-29 16:01:05 +0300

    Add tests

--- vadim.martsun/task_3/src/main/java/randomGenerator/RandomGenerator.java
+++ vadim.martsun/task_3/src/main/java/randomGenerator/RandomGenerator.java
@@ -4,7 +4,6 @@ import objects.Report;
 import java.util.List;
 
 public interface RandomGenerator {
-
     String ALPHABET = "AaBbCcDdEeFfGgHhIiJjKkLlMmNnOo PpQqRrSsTtUuVvWwXxYyZz";
 
     List<Report> generateRandomReports(int count);
