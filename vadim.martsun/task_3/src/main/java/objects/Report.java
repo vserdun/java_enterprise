c6dc368dab892cbd3def2f84a40467d804aa53c6
Vadim Martsun <vadimmartsun@gmail.com>
2019-10-29 15:45:01 +0300

    Add lombok.

--- vadim.martsun/task_3/src/main/java/objects/Report.java
+++ vadim.martsun/task_3/src/main/java/objects/Report.java
@@ -1,48 +1,16 @@
 package objects;
 
+import lombok.*;
 import java.time.LocalDate;
-import java.util.Objects;
 
+@AllArgsConstructor
+@EqualsAndHashCode
+@ToString
+@Data
 public class Report {
     private int id;
     private LocalDate date;
     private String name;
     private String description;
     private Status status;
-
-    public Report(int id, LocalDate date, String name, String description, Status status) {
-        this.id = id;
-        this.date = date;
-        this.name = name;
-        this.description = description;
-        this.status = status;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || getClass() != o.getClass()) return false;
-        Report report = (Report) o;
-        return id == report.id &&
-                date.equals(report.date) &&
-                name.equals(report.name) &&
-                description.equals(report.description) &&
-                status == report.status;
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(id, date, name, description, status);
-    }
-
-    @Override
-    public String toString() {
-        return "Report{" +
-                "id=" + id +
-                ", date=" + date +
-                ", name='" + name + '\'' +
-                ", description='" + description + '\'' +
-                ", status=" + status +
-                '}';
-    }
 }
