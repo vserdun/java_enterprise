2adb549ffe5a915c13533ae0b10ccd7e588777f0
Vadim Martsun <vadimmartsun@gmail.com>
2019-11-09 00:07:47 +0200

    Add some test code

--- vadim.martsun/task_4/src/main/java/Main.java
+++ vadim.martsun/task_4/src/main/java/Main.java
@@ -1,6 +1,17 @@
+import objects.Account;
+import random.RandomBankAccountService;
+import random.RandomBankAccountServiceImpl;
+import service.BankService;
+import service.BankServiceImpl;
+
 public class Main {
 
     public static void main(String ... args){
         System.out.println("Hello");
+        RandomBankAccountService randomBankAccountService = new RandomBankAccountServiceImpl();
+        Account account = randomBankAccountService.getRandomAccounts(1).get(0);
+
+        BankService bankService = new BankServiceImpl();
+        bankService.topUp(account, 105.0f);
     }
 }
