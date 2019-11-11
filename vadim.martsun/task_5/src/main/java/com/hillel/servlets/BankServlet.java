75919a2daad9103d583f7dbf28f552589924d2c9
MathMark <vadimmartsun@gmail.com>
2019-11-11 23:49:14 +0200

    Create BankServlet.java

---
+++ vadim.martsun/task_5/src/main/java/com/hillel/servlets/BankServlet.java
@@ -0,0 +1,31 @@
+package com.hillel.servlets;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class BankServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+    }
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response){
+
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws IOException {
+
+    }
+
+}
