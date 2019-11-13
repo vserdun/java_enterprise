27a9f1952f7dc152ff315efe4608288b2ca92e48
MathMark <vadimmartsun@gmail.com>
2019-11-14 00:44:26 +0200

    Update mapper

--- vadim.martsun/task_5/src/main/java/com/hillel/mapper/BankAccMapper.java
+++
@@ -1,8 +0,0 @@
-package com.hillel.mapper;
-
-import com.hillel.model.BankAcc;
-import com.hillel.model.requests.BankAccRequest;
-
-public interface BankAccMapper {
-    BankAcc map(BankAccRequest request);
-}
--- vadim.martsun/task_5/src/main/java/com/hillel/mapper/MapperImpl.java
+++ vadim.martsun/task_5/src/main/java/com/hillel/mapper/MapperImpl.java
@@ -1,26 +1,36 @@
 package com.hillel.mapper;
 
 import com.hillel.model.BankAcc;
+import com.hillel.model.Database;
 import com.hillel.model.User;
 import com.hillel.model.requests.BankAccRequest;
+import com.hillel.model.requests.UserRequest;
 import lombok.extern.slf4j.Slf4j;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Slf4j
-public class BankAccMapperImpl implements BankAccMapper {
+public class MapperImpl implements Mapper {
+    private Database database = Database.getInstance();
+
     @Override
-    public BankAcc map(BankAccRequest request) {
+    public BankAcc mapBankAccount(BankAccRequest request) {
+        User user = database.getUsers().get(request.getUserId());
+        return new BankAcc(request.getBalance(), user);
+    }
+
+    @Override
+    public User mapUser(UserRequest request) {
         try {
             Date userBirthDate = new SimpleDateFormat("dd.MM.yyyy").parse(request.getUserBirthDate());
             User user = new User();
             user.setFirstName(request.getUserFirstName());
             user.setLastName(request.getUserLastName());
             user.setBirthDate(userBirthDate);
-            return new BankAcc(request.getBalance(), user);
-        } catch (ParseException e) {
-            log.info("Unable to parse date");
+            return user;
+        }catch (ParseException e){
+            log.info("Could not parse date");
             return null;
         }
     }
