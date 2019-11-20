ebba8d453985d1157345803102ffd09ea5ebb607
Sergey <duevsergeywork>
2019-11-20 21:42:54 +0200

    user CRUD forms

--- sergey.duev/task_6/src/main/java/com/hillel/mvc/bank/model/User.java
+++ sergey.duev/task_6/src/main/java/com/hillel/mvc/bank/model/User.java
@@ -2,9 +2,11 @@ package com.hillel.mvc.bank.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class User {
 
     private long id;
