4eabd9001b67e0f923c3a3ae100292aa32277831
MathMark <vadimmartsun@gmail.com>
2019-11-24 21:29:40 +0200

    Create a web page to view a list of accounts.

--- vadim.martsun/task_6/src/main/java/com/hillel/spring/mvc/controllers/AccountPageController.java
+++ vadim.martsun/task_6/src/main/java/com/hillel/spring/mvc/controllers/AccountPageController.java
@@ -1,32 +1,28 @@
 package com.hillel.spring.mvc.controllers;
 
-import com.hillel.spring.mvc.dao.userRepository.UserRepository;
-import com.hillel.spring.mvc.model.Gender;
-import com.hillel.spring.mvc.model.User;
+import com.hillel.spring.mvc.dao.accountRepository.AccountRepository;
+import com.hillel.spring.mvc.model.Account;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.time.LocalDate;
 import java.util.List;
 
 @Controller
-@RequestMapping("/users")
-public class UserPageController {
-
+@RequestMapping("/accounts")
+public class AccountPageController {
     @Autowired
-    UserRepository userRepository;
+    AccountRepository accountRepository;
 
-    @RequestMapping(method = RequestMethod.GET, value = "/usersList")
+    @RequestMapping(method = RequestMethod.GET, value = "/accountList")
     public ModelAndView getUsersList(){
-        List<User> userList = userRepository.getAllUsers();
+        List<Account> accountList = accountRepository.getAll();
 
         ModelAndView modelAndView  = new ModelAndView();
 
-        modelAndView.addObject("users", userList);
-        modelAndView.setViewName("usersPage");
+        modelAndView.addObject("accounts", accountList);
+        modelAndView.setViewName("accountsPage");
         return modelAndView;
     }
 }
