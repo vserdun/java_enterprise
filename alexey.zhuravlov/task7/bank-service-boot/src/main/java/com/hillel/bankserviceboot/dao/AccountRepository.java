2692bc9d57f6a593997d0c61858986a885807ea1
Alex Zhuravlov <38850738+alexzhuravlov@users.noreply.github.com>
2019-11-27 14:48:57 +0200

    Zhuravlov task7 (#41)
    * bank-service-boot start
    
    * fix
    
    * service boot init
    
    * moved to spring boot
    
    * add error page
    
    * custom error page
    
    * add validation
    
    * fix page titles
    
    * fix accountDeposit.jsp

---
+++ alexey.zhuravlov/task7/bank-service-boot/src/main/java/com/hillel/bankserviceboot/dao/AccountRepository.java
@@ -0,0 +1,17 @@
+package com.hillel.bankserviceboot.dao;
+
+import com.hillel.bankserviceboot.model.AccountEntity;
+
+import java.util.List;
+
+public interface AccountRepository {
+    List<AccountEntity> getAccountsList();
+
+    AccountEntity getAccountEntityById(int id);
+
+    void save(AccountEntity accountEntity);
+
+    void update(AccountEntity accountEntity);
+
+    void delete(int id);
+}
