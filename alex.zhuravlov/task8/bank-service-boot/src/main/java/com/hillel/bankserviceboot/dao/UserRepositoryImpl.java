df44889cad51fc1f3fae2e5ec2c0a457caca9407
alexzhravlov <alexzhuravlov13@gmail.com>
2019-12-09 08:21:26 +0200

    first commit

---
+++ alex.zhuravlov/task8/bank-service-boot/src/main/java/com/hillel/bankserviceboot/dao/UserRepositoryImpl.java
@@ -0,0 +1,47 @@
+package com.hillel.bankserviceboot.dao;
+
+
+import com.hillel.bankserviceboot.model.UserEntity;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+@Repository
+public class UserRepositoryImpl implements UserRepository {
+
+    private int currentUserId = 1;
+    private Map<Integer, UserEntity> usersMap = new ConcurrentHashMap<>();
+
+    @Override
+    public List<UserEntity> getUsersList() {
+        return new ArrayList<>(usersMap.values());
+    }
+
+    @Override
+    public UserEntity getUserEntityById(int id) {
+        return usersMap.get(id);
+    }
+
+    @Override
+    public void save(UserEntity userEntity) {
+        if (usersMap.get(userEntity.getUserId()) == null) {
+            userEntity.setUserId(currentUserId);
+            currentUserId++;
+        }
+
+        usersMap.put(userEntity.getUserId(), userEntity);
+    }
+
+    @Override
+    public void update(UserEntity userEntity) {
+        usersMap.put(userEntity.getUserId(), userEntity);
+    }
+
+    @Override
+    public void delete(int id) {
+        usersMap.remove(id);
+    }
+}
