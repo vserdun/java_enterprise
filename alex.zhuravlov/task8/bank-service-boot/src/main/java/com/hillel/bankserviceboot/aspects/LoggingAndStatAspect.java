ce6810609add0ca7f1f5e7dc98610e04e2acb8a0
alexzhravlov <alexzhuravlov13@gmail.com>
2019-12-09 09:17:36 +0200

    counts

--- alex.zhuravlov/task8/bank-service-boot/src/main/java/com/hillel/bankserviceboot/aspects/LoggingAndStatAspect.java
+++ alex.zhuravlov/task8/bank-service-boot/src/main/java/com/hillel/bankserviceboot/aspects/LoggingAndStatAspect.java
@@ -10,6 +10,12 @@ import org.springframework.stereotype.Component;
 @Slf4j
 public class LoggingAndStatAspect {
 
+    private int createdUsers = 0;
+    private int deletedUsers = 0;
+
+    private int createdAccounts = 0;
+    private int deletedAccounts = 0;
+
     @Before("PointCuts.serviceOrRepository()")
     public void enteringServiceOrRepository(JoinPoint joinPoint)
     {
@@ -32,5 +38,31 @@ public class LoggingAndStatAspect {
             log.info("Object {}", object);
         }
     }
+
+    @After("execution (* com.hillel.bankserviceboot.service.*.addUser(..))")
+    public void afterUserAddded(JoinPoint joinPoint){
+        createdUsers++;
+        log.info("Created users:{}", createdUsers);
+    }
+
+    @After("execution (* com.hillel.bankserviceboot.service.*.deleteUser(..))")
+    public void afterUserDeleted(JoinPoint joinPoint){
+        deletedUsers++;
+        log.info("Deleted users:{}", deletedUsers);
+
+    }
+
+    @After("execution (* com.hillel.bankserviceboot.service.*.addAccount(..))")
+    public void afterAccountAddded(JoinPoint joinPoint){
+        createdAccounts++;
+        log.info("Created accounts:{}", createdAccounts);
+    }
+
+    @After("execution (* com.hillel.bankserviceboot.service.*.deleteAccount(..))")
+    public void afterAccountDeleted(JoinPoint joinPoint){
+        deletedAccounts++;
+        log.info("Deleted accounts:{}", deletedAccounts);
+    }
+
 
 }
