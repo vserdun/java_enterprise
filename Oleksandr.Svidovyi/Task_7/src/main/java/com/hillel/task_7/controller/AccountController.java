25c2a3789628065d2e8b458e7746944d7d1b0aea
Svidic111 <35878170+Svidic111@users.noreply.github.com>
2020-01-17 17:52:17 +0200

    Sidovyi_Task_7 (#54)
    * 24.12.2019 corrections
    
    * Svidovyi Task_7
    
    * Svidovyi Task_7
    
    * Delete executionHistory.bin
    
    * Delete executionHistory.lock
    
    * Delete last-build.bin
    
    * Delete fileHashes.bin
    
    * Delete fileHashes.lock
    
    * Delete gc.properties
    
    * Delete gc.properties
    
    * Delete buildOutputCleanup.lock
    
    * Delete cache.properties
    
    * Delete outputFiles.bin
    
    * Delete gradle-wrapper.jar
    
    * Delete gradle-wrapper.properties
    
    * Delete gradlew
    
    * Delete gradlew.bat
    
    * Svidovyi Task_7
    
    * Svidovyi Task_7

---
+++ Oleksandr.Svidovyi/Task_7/src/main/java/com/hillel/task_7/controller/AccountController.java
@@ -0,0 +1,55 @@
+package com.hillel.task_7.controller;
+
+import com.hillel.task_7.model.Account;
+import com.hillel.task_7.service.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/clients/{id}/account")
+public class AccountController {
+    @Autowired
+    private ClientService clientService;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/list")
+    public ModelAndView getAccounts(@PathVariable int id) {
+        List<Account> accounts = clientService.getAccountsByClientId(id);
+
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("clientAccounts", accounts);
+        modelAndView.setViewName("clientAccountsList");
+
+        return modelAndView;
+    }
+
+
+    @RequestMapping(method = RequestMethod.GET, value = "/add")
+    public String addAccount(Model model) {
+        model.addAttribute("clientAccountAttribute", new Account());
+        return "addAccount";
+    }
+
+
+    @RequestMapping(method = RequestMethod.POST, value = "/addAccount")
+    public String saveAccount(@ModelAttribute("clientAccountAttribute") @Validated Account account, @PathVariable int id) {
+        clientService.saveAccount(id, account);
+
+        return "redirect:list";
+    }
+
+    @RequestMapping(method = RequestMethod.GET, value = "/deleteAccount")
+    public String deleteClient(@PathVariable int id, @ModelAttribute("accountAttribute") @Validated Account account) {
+        clientService.removeAccount(id, account);
+
+        return "redirect:list";
+    }
+}
---
+++ Oleksandr.Svidovyi/Task_7/src/main/java/com/hillel/task_7/model/Account.java
@@ -0,0 +1,16 @@
+package com.hillel.task_7.model;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.PositiveOrZero;
+
+
+@Data
+public class Account {
+    @PositiveOrZero
+    private long balance;
+
+    @NotBlank
+    private String currency;
+}
