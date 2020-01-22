d8092612121637087ceca31b427e8c7a9d79a08f
Svidic111 <35878170+Svidic111@users.noreply.github.com>
2020-01-22 17:40:26 +0200

    Svidovyi task 8 (#55)
    * 24.12.2019 corrections
    
    * Task 8
    
    * Task 8
    
    * Delete gradlew.bat
    
    * Delete gradlew
    
    * Delete gradle-wrapper.properties
    
    * Delete gradle-wrapper.jar
    
    * Delete gc.properties
    
    * Delete outputFiles.bin
    
    * Delete cache.properties
    
    * Delete buildOutputCleanup.lock
    
    * Delete executionHistory.bin
    
    * Delete executionHistory.lock
    
    * Delete gc.properties
    
    * Delete last-build.bin
    
    * Delete fileHashes.bin
    
    * Delete fileHashes.lock

---
+++ Oleksandr.Svidovyi/Task_8/src/main/java/com/hillel/task_8/Task8Application.java
@@ -0,0 +1,15 @@
+package com.hillel.task_8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@SpringBootApplication
+@EnableAspectJAutoProxy
+public class Task8Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Task8Application.class, args);
+	}
+
+}
---
+++ Oleksandr.Svidovyi/Task_8/src/main/java/com/hillel/task_8/repository/ClientRepositoryImpl.java
@@ -0,0 +1,39 @@
+package com.hillel.task_8.repository;
+
+import com.hillel.task_8.model.Client;
+import org.springframework.stereotype.Repository;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+@Repository
+public class ClientRepositoryImpl implements ClientRepository {
+    private HashMap<Integer, Client> clients = new HashMap<>();
+    private int currentId = 1;
+
+
+    @Override
+    public Client getClientById(int id) {
+        return clients.getOrDefault(id, null);
+    }
+
+
+    @Override
+    public List<Client> getClientsList() {
+        return new ArrayList<>(clients.values());
+    }
+
+    @Override
+    public void save(Client client) {
+        client.setId(currentId);
+        clients.put(currentId, client);
+        currentId++;
+    }
+
+
+    @Override
+    public void deleteClientById(int id) {
+        clients.remove(id);
+    }
+}
